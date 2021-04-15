921db230a468f0a18634c89ae8bbab311f869826
Florian Westphal <fw@strlen.de>
2021-04-16 07:44:54 +0800

    mptcp: tag sequence_seq with socket state
    Git-commit: df00b087da24c0b5341178bbd5353101c7cef98f
    Message-Id: <df00b087da24c0b5341178bbd5353101c7cef98f.1637073216.git.geliang.tang@suse.com>
    References: bsn#2
    Patch-mainline: v5.13-rc1
    
    Paolo Abeni suggested to avoid re-syncing new subflows because
    they inherit options from listener. In case options were set on
    listener but are not set on mptcp-socket there is no need to
    do any synchronisation for new subflows.
    
    This change sets sockopt_seq of new mptcp sockets to the seq of
    the mptcp listener sock.
    
    Subflow sequence is set to the embedded tcp listener sk.
    Add a comment explaing why sk_state is involved in sockopt_seq
    generation.
    
    Acked-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>
    
    Patch-name: patches.suse/v5.13-rc1-mptcp-tag-sequence_seq-with-socket-state.patch

--- net/mptcp/protocol.h
+++ net/mptcp/protocol.h
@@ -257,6 +257,8 @@ struct mptcp_sock {
 		u64	time;	/* start time of measurement window */
 		u64	rtt_us; /* last maximum rtt of subflows */
 	} rcvq_space;
+
+	u32 setsockopt_seq;
 };
 
 #define mptcp_lock_sock(___sk, cb) do {					\
@@ -414,6 +416,8 @@ struct mptcp_subflow_context {
 
 	long	delegated_status;
 	struct	list_head delegated_node;   /* link into delegated_action, protected by local BH */
+
+	u32 setsockopt_seq;
 
 	struct	sock *tcp_sock;	    /* tcp sk backpointer */
 	struct	sock *conn;	    /* parent mptcp_sock */
