e01897ac17a745c92477e797de6e2f495fda7b10
Kai Liu <kai.liu@suse.com>
2021-10-07 14:45:21 +0800

    packaging: add essential files for building kernel packages
    config.conf:
        Definition of kernel flavors and config files.
    
    config/x86_64/default:
    config/arm64/defult:
        Kernel config files for each flavor and hardware architecture. They
        are extracted from openEuler kernel config file:
          arch/{arm64,x86}/configs/openeuler_defconfig
    
    rpm/config.sh
        Base configuration of kernel RPM, such as kernel base version, build
        project on OBS etc.
    
    rpm/kernel-binary.spec.in:
        Spec file template for the kernel-$flavor packages. Change
        dependencies from SUSE package names to openEuler names. Some
        dependencies, namely suse-kernel-rpm-scriptlets and
        suse-module-tools, don't exsit in openEuler so are commented out for
        now. They may need to be ported later.
    
    rpm/kernel-source.spec.in:
        Spec file template for the kernel-source package. Add patch archives
        for openEuler specific patches.
    
    series.conf:
        Patch applying sequence, empty for now.
    
    supported.conf:
        Kernel module support flags, empty for now.
    
    Change-Id: Ia09b88b00e40e41c00b13755982e184c50d41df3

---
+++ rpm/config.sh
@@ -0,0 +1,18 @@
+# The version of the main tarball to use
+SRCVERSION=4.19
+# variant of the kernel-source package, either empty or "-rt"
+VARIANT=
+# enable kernel module compression
+COMPRESS_MODULES="xz"
+# Use new style livepatch package names
+#LIVEPATCH=livepatch
+# Compile binary devicetrees for Leap
+BUILD_DTBS="Yes"
+# buildservice projects to build the kernel against
+OBS_PROJECT=openEuler.org:20.03:LTS:SP2
+OBS_PROJECT_ARM=openEuler.org:20.03:LTS:SP2
+# Bugzilla info
+#BUGZILLA_SERVER="apibugzilla.suse.com"
+#BUGZILLA_PRODUCT="SUSE Linux Enterprise Server 15 SP3"
+# Check the sorted patches section of series.conf
+SORT_SERIES=yes
