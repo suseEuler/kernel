34b0526570c61fda19a9ce0e01e4b34df22b15e1
Zhenguo Yao <yaozhenguo1@gmail.com>
2021-11-06 04:43:28 +0800

    hugetlbfs: extend the definition of hugepages parameter to
    From b5389086ad7be0453c55e0069a89856d1fbdf605 Mon Sep 17 00:00:00 2001
     support node allocation
    Git-commit: b5389086ad7be0453c55e0069a89856d1fbdf605
    Patch-mainline: v5.16-rc1
    References: bsn#12
    
    We can specify the number of hugepages to allocate at boot.  But the
    hugepages is balanced in all nodes at present.  In some scenarios, we
    only need hugepages in one node.  For example: DPDK needs hugepages
    which are in the same node as NIC.
    
    If DPDK needs four hugepages of 1G size in node1 and system has 16 numa
    nodes we must reserve 64 hugepages on the kernel cmdline.  But only four
    hugepages are used.  The others should be free after boot.  If the
    system memory is low(for example: 64G), it will be an impossible task.
    
    So extend the hugepages parameter to support specifying hugepages on a
    specific node.  For example add following parameter:
    
      hugepagesz=1G hugepages=0:1,1:3
    
    It will allocate 1 hugepage in node0 and 3 hugepages in node1.
    
    Link: https://lkml.kernel.org/r/20211005054729.86457-1-yaozhenguo1@gmail.com
    Signed-off-by: Zhenguo Yao <yaozhenguo1@gmail.com>
    Reviewed-by: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Zhenguo Yao <yaozhenguo1@gmail.com>
    Cc: Dan Carpenter <dan.carpenter@oracle.com>
    Cc: Nathan Chancellor <nathan@kernel.org>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Guoqing Jiang <guoqing.jiang@suse.com>
    
    Patch-name: patches.mainline/v5.16-rc1-hugetlbfs-extend-the-definition-of-hugepages-paramet.patch

--- include/linux/hugetlb.h
+++ include/linux/hugetlb.h
@@ -578,6 +578,7 @@ struct hstate {
 	unsigned long nr_overcommit_huge_pages;
 	struct list_head hugepage_activelist;
 	struct list_head hugepage_freelists[MAX_NUMNODES];
+	unsigned int max_huge_pages_node[MAX_NUMNODES];
 	unsigned int nr_huge_pages_node[MAX_NUMNODES];
 	unsigned int free_huge_pages_node[MAX_NUMNODES];
 	unsigned int surplus_huge_pages_node[MAX_NUMNODES];
@@ -608,8 +609,9 @@ int huge_add_to_page_cache(struct page *page, struct address_space *mapping,
 			pgoff_t idx);
 
 /* arch callback */
-int __init __alloc_bootmem_huge_page(struct hstate *h);
-int __init alloc_bootmem_huge_page(struct hstate *h);
+int __init __alloc_bootmem_huge_page(struct hstate *h, int nid);
+int __init alloc_bootmem_huge_page(struct hstate *h, int nid);
+bool __init hugetlb_node_alloc_supported(void);
 
 void __init hugetlb_add_hstate(unsigned order);
 bool __init arch_hugetlb_valid_size(unsigned long size);
