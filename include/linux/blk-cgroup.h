0c9f83da0bedfb4e3ca17e24636178606c4592fc
Zhihao Cheng <chengzhihao1@huawei.com>
2021-12-31 15:59:55 +0800

    kabi: Add kabi reservation for storage module
    From dde4fe567523327d09054824030fb2d22e5009c7 Mon Sep 17 00:00:00 2001
    Patch-mainline: Not yet, from openEuler
    References: bsn#22
    openEuler-commit: dde4fe567523327d09054824030fb2d22e5009c7
    
    hulk inclusion
    category: feature
    bugzilla: 185747 https://gitee.com/openeuler/kernel/issues/I4OUFN
    CVE: NA
    
    -------------------------------
    
    Introduce kabi for storage module.
    
    Signed-off-by: Zhihao Cheng <chengzhihao1@huawei.com>
    Reviewed-by: Jason Yan <yanaijie@huawei.com>
    Acked-by: Xie XiuQi <xiexiuqi@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Signed-off-by: Guoqing Jiang <guoqing.jiang@suse.com>
    
    Patch-name: patches.openEuler/12738-kabi-Add-kabi-reservation-for-storage-module.patch

--- include/linux/blk-cgroup.h
+++ include/linux/blk-cgroup.h
@@ -14,6 +14,7 @@
  * 	              Nauman Rafique <nauman@google.com>
  */
 
+#include <linux/kabi.h>
 #include <linux/cgroup.h>
 #include <linux/percpu.h>
 #include <linux/percpu_counter.h>
@@ -63,6 +64,11 @@ struct blkcg {
 #ifdef CONFIG_CGROUP_WRITEBACK
 	struct list_head		cgwb_list;
 #endif
+
+	KABI_RESERVE(1)
+	KABI_RESERVE(2)
+	KABI_RESERVE(3)
+	KABI_RESERVE(4)
 };
 
 struct blkg_iostat {
@@ -174,6 +180,11 @@ struct blkcg_policy {
 	blkcg_pol_free_pd_fn		*pd_free_fn;
 	blkcg_pol_reset_pd_stats_fn	*pd_reset_stats_fn;
 	blkcg_pol_stat_pd_fn		*pd_stat_fn;
+
+	KABI_RESERVE(1)
+	KABI_RESERVE(2)
+	KABI_RESERVE(3)
+	KABI_RESERVE(4)
 };
 
 extern struct blkcg blkcg_root;
