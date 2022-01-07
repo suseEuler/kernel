5d282f2b728f0d4d141fbc6f58d91f1bd8ddbac0
Zheng Zengkai <zhengzengkai@huawei.com>
2022-01-07 23:05:17 +0800

    KABI: Add KABI_AUX_PTR extenstions to some more base
    From a62969d3b65754dacfb6898914764b4464249f18 Mon Sep 17 00:00:00 2001
     structures
    Patch-mainline: Not yet, from openEuler
    References: bsn#22
    openEuler-commit: a62969d3b65754dacfb6898914764b4464249f18
    
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I4JBL0
    CVE: NA
    
    ------------------------------
    
    Add KABI_AUX_PTR extenstions to the following base structures
    before KABI freeze:
    
    struct device_driver
    struct class
    struct device
    struct hrtimer
    struct ipmi_smi_handlers
    struct net_device
    
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Reviewed-by: Xie XiuQi <xiexiuqi@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Signed-off-by: Guoqing Jiang <guoqing.jiang@suse.com>
    
    Patch-name: patches.openEuler/12748-KABI-Add-KABI_AUX_PTR-extenstions-to-some-more-base.patch

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1647,6 +1647,12 @@ enum netdev_ml_priv_type {
 };
 
 /**
+ * struct net_device_extended_resvd - KABI extension struct
+ */
+struct net_device_extended_resvd {
+};
+
+/**
  *	struct net_device - The DEVICE structure.
  *
  *	Actually, this whole structure is a big mistake.  It mixes I/O
@@ -2222,6 +2228,7 @@ struct net_device {
 	KABI_RESERVE(6)
 	KABI_RESERVE(7)
 	KABI_RESERVE(8)
+	KABI_AUX_PTR(net_device_extended)
 };
 #define to_net_dev(d) container_of(d, struct net_device, dev)
 
