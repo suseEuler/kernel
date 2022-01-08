62d3eff5f25261b26191e974daa74f30426b9441
Kemeng Shi <shikemeng@huawei.com>
2022-01-08 18:52:33 +0800

    acpi/numa: memorize node type from SRAT table
    From 1c76b8cf0bd5dbcd0ebb576384e2f3af77aaec99 Mon Sep 17 00:00:00 2001
    Patch-mainline: Not yet, from openEuler
    References: bsn#22
    openEuler-commit: 1c76b8cf0bd5dbcd0ebb576384e2f3af77aaec99
    Modified-by-SEL: Yes, refreshed due to context change
    
    euleros inclusion
    category: feature
    feature: etmem
    bugzilla: https://gitee.com/openeuler/kernel/issues/I4OODH?from=project-issue
    CVE: NA
    
    -------------------------------------------------
    
    Driver dax_kmem will export pmem as a NUMA node. This patch will
    record node consists of persistent memory for futher use.
    
    Signed-off-by: Kemeng Shi <shikemeng@huawei.com>
    Reviewed-by: louhongxiang <louhongxiang@huawei.com>
    Reviewed-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Signed-off-by: Guoqing Jiang <guoqing.jiang@suse.com>
    
    Patch-name: patches.euleros/12890-acpi-numa-memorize-node-type-from-SRAT-table.patch

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -9164,6 +9164,19 @@ bool take_page_off_buddy(struct page *page)
 }
 #endif
 
+#ifdef CONFIG_NUMA
+enum node_type nodes_type[MAX_NUMNODES];
+
+void set_node_type(int nid, enum node_type type)
+{
+	nodes_type[nid] = type;
+}
+enum node_type get_node_type(int nid)
+{
+	return nodes_type[nid];
+}
+#endif
+
 #ifdef CONFIG_ZONE_DMA
 bool has_managed_dma(void)
 {
