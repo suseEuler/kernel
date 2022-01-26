c3dffb0acbcb91d261e2509707799ad5a47fe792
Geliang Tang <geliang.tang@suse.com>
2022-01-26 12:13:06 +0800

    scripts: add Modified-by-SEL tag from openEuler-commit
    This script parses the openEuler-commit id from the given patch, and
    adds the Modified-by-SEL tag with it.
    
    Usage:
    
     ./scripts/Modified-by-SEL-from-openEuler-commit.sh patches.mainline/a.patch
     for p in `cat patches.list`; do ./scripts/Modified-by-SEL-from-openEuler-commit.sh $p; done
    
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>
    Change-Id: I174a436268e5c3e7bdde607e1b05b0111b2ed799

---
+++ scripts/Modified-by-SEL-from-openEuler-commit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# != 1 ]; then
+	exit
+fi
+
+patch=$1
+
+openeuler_commit=$(grep -r "openEuler-commit:" $patch | awk '{ print $2 }')
+if [ "$openeuler_commit" = "" ]; then
+	tag="No"
+else
+	patches_dir=$(dirname $patch)
+	if [ "$patches_dir" = "patches.mainline" ]; then
+		tag="Yes, modified according to openEuler commit $openeuler_commit"
+		sed -i '/openEuler-commit:/'d $patch
+	elif [ "$patches_dir" = "patches.openEuler" ]; then
+		tag="FIXME"
+	elif [ "$patches_dir" = "patches.euleros" ]; then
+		tag="FIXME"
+	else
+		tag="No"
+	fi
+fi
+sed -i '/References:/a\Modified-by-SEL: '"$tag"'' $patch
